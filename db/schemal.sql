446edbcc2a368f29b36407bebcd9a96ba4a72e09
Baer Lanfried <blanfried@yahoo.com>
2022-12-04 07:36:30 +0000

    created three tables (department, role, employee) in schema.sql

--- db/schemal.sql
+++ db/schemal.sql
@@ -1,2 +1,29 @@
 DROP DATABASE IF EXISTS tracker_db;
 CREATE DATABASE tracker_db;
+
+USE tracker_db;
+
+CREATE TABLE department (
+  id int AUTO_INCREMENT NOT NULL,
+  name varchar(30) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  INDEX dep_ind (department_id),
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INT,
+  PRIMARY KEY(id)
+);
